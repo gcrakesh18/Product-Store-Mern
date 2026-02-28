6abde03aec1e78848079d2d664519f8feaec9601
gcrakesh18 <rakeshdevanga12@gmail.com>
2026-02-28 12:43:13 +0530

    NavBar improvement

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import { PlusSquareIcon } from '@chakra-ui/icons'
 import { IoMoon } from 'react-icons/io5'
 import { LuSun } from 'react-icons/lu'
-
+// nav bar 
 const Navbar = () => {
     const { colorMode, toggleColorMode } = useColorMode();
     return (
